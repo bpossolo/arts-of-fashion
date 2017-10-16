b19ab7aee952c969954c47b1efec8c911ae5d4eb
Benjamin Possolo <bpossolo@gmail.com>
2017-10-15 23:48:34 -0700

    fix(video): use vimeo instead of native video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { SafeStyle, DomSanitizer } from '@angular/platform-browser';
 import * as studentData from '../assets/json/students.json';
 import * as judgeData from '../assets/json/judges.json';
-
-interface Student {
-  image: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -17,33 +13,11 @@ export class AppComponent {
   students = studentData;
   judges = judgeData;
 
-  @ViewChild('teaser')
-  videoEl: ElementRef;
-
-  playing = false;
-
   constructor(private sanitizer: DomSanitizer) {}
 
-  playVideo(): void {
-    const video = this.getVideoEl();
-    video.play();
-    this.playing = true;
-  }
-
-  onVideoEnded(): void {
-    this.playing = false;
-    const video = this.getVideoEl();
-    video.load();
-  }
-
-  getSketchBackgroundImageStyle(student: Student): SafeStyle {
-    const url = student.image;
+  getSketchBackgroundImageStyle(url: string): SafeStyle {
     const style = `url('${url}')`;
     const safeStyle = this.sanitizer.bypassSecurityTrustStyle(style);
     return safeStyle;
-  }
-
-  private getVideoEl(): HTMLVideoElement {
-    return this.videoEl.nativeElement as HTMLVideoElement;
   }
 }
