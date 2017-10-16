a73b9bc6e302ea428d92503b67bfb7f5bf417a9f
Benjamin Possolo <bpossolo@gmail.com>
2017-10-15 18:50:03 -0700

    feat(landing page): additional updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { SafeStyle, DomSanitizer } from '@angular/platform-browser';
 import * as studentData from '../assets/json/students.json';
+import * as judgeData from '../assets/json/judges.json';
+
+interface Student {
+  image: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -9,4 +15,35 @@ import * as studentData from '../assets/json/students.json';
 export class AppComponent {
   
   students = studentData;
+  judges = judgeData;
+
+  @ViewChild('teaser')
+  videoEl: ElementRef;
+
+  playing = false;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  playVideo(): void {
+    const video = this.getVideoEl();
+    video.play();
+    this.playing = true;
+  }
+
+  onVideoEnded(): void {
+    this.playing = false;
+    const video = this.getVideoEl();
+    video.load();
+  }
+
+  getSketchBackgroundImageStyle(student: Student): SafeStyle {
+    const url = student.image;
+    const style = `url('${url}')`;
+    const safeStyle = this.sanitizer.bypassSecurityTrustStyle(style);
+    return safeStyle;
+  }
+
+  private getVideoEl(): HTMLVideoElement {
+    return this.videoEl.nativeElement as HTMLVideoElement;
+  }
 }
